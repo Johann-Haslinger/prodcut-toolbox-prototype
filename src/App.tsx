7799f5890614584b1909ce375889cf701bfde7f3
Johann Haslinger <johann.hsr@icloud.com>
2025-03-29 12:18:31 +0100

    chore: remove Button component from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-import { EyeClosed } from "lucide-react";
-import { Button } from "./components/atoms";
-import { ButtonColor } from "./types/enums";
-
 function App() {
-  return (
-    <div className="p-40">
-      <Button icon={EyeClosed} color={ButtonColor.BLUE} tooltip="hallo" />
-    </div>
-  );
+  return null;
 }
 
 export default App;
