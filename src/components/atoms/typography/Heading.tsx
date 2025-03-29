f963430996fbf78446c70e5c188f6f9c9f644b6f
Johann Haslinger <johann.hsr@icloud.com>
2025-03-29 12:27:51 +0100

    feat: migrate IconButton component to new directory structure and update stories

---
+++ src/components/atoms/typography/Heading.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const Heading = styled.p`
+  ${tw`text-4xl font-bold text-[#1A1B1D] dark:text-white`}
+`;
+
+export default Heading;
---
+++ src/components/atoms/typography/SubHeading.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+
+const SubHeading = styled.p`
+  ${tw`text-base text-black/50 dark:text-white/50`};
+`;
+
+export default SubHeading;
